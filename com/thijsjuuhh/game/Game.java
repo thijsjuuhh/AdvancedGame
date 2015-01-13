8244498f69fac8480867a834b52dbe7c8c45c14d
thijsjuuhh <thijsalberts@home.nl>
2015-01-13 14:57:52 +0100

    General startup

--- com/thijsjuuhh/game/Game.java
+++ com/thijsjuuhh/game/Game.java
@@ -1,9 +1,37 @@
 package com.thijsjuuhh.game;
 
-public class Game {
+public class Game implements Runnable {
+
+	public static boolean running = false;
+	public static Thread thread;
 
 	public static void main(String[] args) {
 		System.out.println("System running");
 	}
-	
+
+	public Game() {
+		thread = new Thread(this, Reference.GameNames.gameName);
+
+	}
+
+	public synchronized void start() {
+		running = true;
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (running)
+			System.out.println("System running");
+	}
+
 }
---
+++ com/thijsjuuhh/game/Reference.java
@@ -0,0 +1,7 @@
+package com.thijsjuuhh.game;
+
+public final class Reference {
+	public class GameNames {
+		public static final String gameName = "Advanced game";
+	}
+}
